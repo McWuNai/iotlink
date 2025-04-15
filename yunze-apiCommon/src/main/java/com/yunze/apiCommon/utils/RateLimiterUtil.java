7f790ae9e54bb8e6bdf679bddabbc78cd86ad096
McWuNai <3331547937@qq.com>
2025-04-15 10:14:33 +0800

    fix: 限流逻辑更改(取消总限，设为每个sso接口各自单限)

--- yunze-apiCommon/src/main/java/com/yunze/apiCommon/utils/RateLimiterUtil.java
+++ yunze-apiCommon/src/main/java/com/yunze/apiCommon/utils/RateLimiterUtil.java
@@ -18,29 +18,31 @@ public class RateLimiterUtil {
     @PostConstruct
     public void init() {
         // 这个方法会在类初始化后立即执行
-        this.tokenCountMap.put("queryFlow", 1.0);
-        this.tokenCountMap.put("queryCardActiveTime", 1.0);
-        this.tokenCountMap.put("queryFlowHis", 1.0);
-        this.tokenCountMap.put("queryCardStatus", 1.0);
-        this.tokenCountMap.put("changeCardStatus", 1.0);
-        this.tokenCountMap.put("changeCardStatusFlexible", 1.0);
-        this.tokenCountMap.put("queryRealNameStatus", 1.0);
-        this.tokenCountMap.put("queryAPNInfo", 1.0);
+        this.tokenCountMap.put("queryFlow", 5.0);
+        this.tokenCountMap.put("queryCardActiveTime", 5.0);
+        this.tokenCountMap.put("queryFlowHis", 5.0);
+        this.tokenCountMap.put("queryCardStatus", 5.0);
+        this.tokenCountMap.put("changeCardStatus", 10.0);
+        this.tokenCountMap.put("changeCardStatusFlexible", 10.0);
+        this.tokenCountMap.put("queryRealNameStatus", 5.0);
+        this.tokenCountMap.put("queryAPNInfo", 5.0);
         this.tokenCountMap.put("SpeedLimit", 1.0);
-        this.tokenCountMap.put("FunctionApnStatus", 1.0);
-        this.tokenCountMap.put("MachineCardBinding", 0.0);
-        this.tokenCountMap.put("realNameRemove", 0.0);
-        this.tokenCountMap.put("queryBatchFlow", 1.0);
+        this.tokenCountMap.put("FunctionApnStatus", 5.0);
+        this.tokenCountMap.put("MachineCardBinding", 5.0);
+        this.tokenCountMap.put("realNameRemove", 5.0);
+        this.tokenCountMap.put("queryBatchFlow", 5.0);
+        this.tokenCountMap.put("wsQueryIdByName", 10.0);
+        this.tokenCountMap.put("addRatePlan", 10.0);
 
         System.out.println("tokenCountMap 初始化完成");
     }
 
     // 全局限流器，每个账号每秒最多10个请求
-    public RateLimiter getGlobalRateLimiter(String cd_userName) {
-        return globalRateLimiters.computeIfAbsent(cd_userName, userName -> RateLimiter.create(10.0));
-    }
+    /*public RateLimiter getGlobalRateLimiter(String cd_userName) {
+        return globalRateLimiters.computeIfAbsent(cd_userName, userName -> RateLimiter.create(130.0));
+    }*/
 
-    // 特定方法限流器，每个账号每秒最多1个请求
+    // 特定方法限流器，每个账号每秒最多N个请求
     public RateLimiter getSpecificMethodRateLimiter(String cd_userName, String methodName) {
         // 使用用户名和方法名作为复合键
         String compositeKey = buildCompositeKey(cd_userName, methodName);
@@ -56,7 +58,7 @@ public class RateLimiterUtil {
         String userNameStr = cd_userName.toString();
 
         // 获取全局限流器
-        RateLimiter globalRateLimiter = getGlobalRateLimiter(userNameStr);
+        //RateLimiter globalRateLimiter = getGlobalRateLimiter(userNameStr);
 
         //获取方法名对应的令牌预设值
 
@@ -67,14 +69,14 @@ public class RateLimiterUtil {
         specificMethodRateLimiter.acquire(1);
 
         // 尝试获取全局的令牌，若无足够令牌，则等待
-        globalRateLimiter.acquire(1);
+        //globalRateLimiter.acquire(1);
     }
 
     // 用于单重限流检查
-    public void acquireForNormalMethod(Object cd_userName) {
+    /*public void acquireForNormalMethod(Object cd_userName) {
         RateLimiter globalRateLimiter = getGlobalRateLimiter(cd_userName.toString());
 
         // 尝试获取全局的令牌，若无足够令牌，则等待
         globalRateLimiter.acquire(1);
-    }
+    }*/
 }
