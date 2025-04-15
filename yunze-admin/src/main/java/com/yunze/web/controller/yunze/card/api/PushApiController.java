fb8998a19dbb13f8ba2a3b0cde681039983c51bc
McWuNai <3331547937@qq.com>
2025-04-15 10:15:27 +0800

    fix: 自动化规则转换类型不匹配

--- yunze-admin/src/main/java/com/yunze/web/controller/yunze/card/api/PushApiController.java
+++ yunze-admin/src/main/java/com/yunze/web/controller/yunze/card/api/PushApiController.java
@@ -1,5 +1,6 @@
 package com.yunze.web.controller.yunze.card.api;
 
+import cn.hutool.json.JSONUtil;
 import com.yunze.common.mapper.yunze.YzCardMapper;
 import com.yunze.common.utils.ServletUtils;
 import com.yunze.common.utils.ip.IpUtils;
@@ -32,7 +33,8 @@ public class PushApiController {
     @PostMapping ("/getCardDataUsage")
     public void getFlowUsed(@RequestBody Map<String, Object> requestBody) {
         // 因为"data"字段是嵌套对象，所以需要用Map来接收
-        Map<String, Object> data = (Map<String, Object>) requestBody.get("data");
+        Map<String, Object> data = objectToMap(requestBody.get("data"));
+        //Map<String, Object> data = (Map<String, Object>) requestBody.get("data");
         try {
             HashMap<String, Object> Parammap = new HashMap<>();
             Parammap.put("iccid", data.get("iccid"));
@@ -61,7 +63,12 @@ public class PushApiController {
                         log.error("接口超频返回暂无数据返回，请稍后重试！");
                     }
                 } else {
-                    String statusVal = cd_status.equals("2") ? "已停用" : cd_status.equals("3") ? "已删除" : "状态未知";
+                    String statusVal = null;
+                    if (cd_status != null) {
+                        statusVal = cd_status.equals("2") ? "已停用" : cd_status.equals("3") ? "已删除" : "状态未知";
+                    } else {
+                        statusVal = "状态未知";
+                    }
                     log.info("同步用量 操作失败！" + " 通道 [" + statusVal + "]");
                 }
             } else {
@@ -84,10 +91,9 @@ public class PushApiController {
         yzCardServiceImpl.pushApi(outData);
     }*/
 
-    @PostMapping ("/getCardStatus")
-    public void getCardStatus(@RequestBody Map<String, Object> requestBody) {
-        // 因为"data"字段是嵌套对象，所以需要用Map来接收
-        Map<String, Object> data = (Map<String, Object>) requestBody.get("data");
+    @PostMapping ("/activeCardStatus")
+    public void activeCardStatus(@RequestBody Map<String, Object> requestBody) {
+        Map<String, Object> data = objectToMap(requestBody.get("data"));
         /*Map<String, Object> outData = new HashMap<>();
         outData.put("iccid", data.get("iccid"));
         outData.put("currentState", data.get("currentState"));
@@ -97,8 +103,38 @@ public class PushApiController {
         String status_id = data.get("currentState").toString();
         Map<String, Object> Upd_Map = new HashMap<>();
         Upd_Map.put("iccid",data.get("iccid"));
-        Upd_Map.put("status_id",status_id);
+        Upd_Map.put("status_id", "1");
         Upd_Map.put("status_ShowId",getShowStatIdArr.GetShowStatId(status_id));
         yzCardMapper.updStatusId(Upd_Map);
+    }
+
+    @PostMapping ("/stopCardStatus")
+    public void stopCardStatus(@RequestBody Map<String, Object> requestBody) {
+        Map<String, Object> data = objectToMap(requestBody.get("data"));
+        /*Map<String, Object> outData = new HashMap<>();
+        outData.put("iccid", data.get("iccid"));
+        outData.put("currentState", data.get("currentState"));
+        outData.put("api_type", 2);
+        yzCardServiceImpl.pushApi(outData);*/
+
+        String status_id = data.get("currentState").toString();
+        Map<String, Object> Upd_Map = new HashMap<>();
+        Upd_Map.put("iccid",data.get("iccid"));
+        Upd_Map.put("status_id", "2");
+        Upd_Map.put("status_ShowId",getShowStatIdArr.GetShowStatId(status_id));
+        yzCardMapper.updStatusId(Upd_Map);
+    }
+
+    private Map<String, Object> objectToMap (Object dataObj) {
+        // 因为"data"字段是嵌套对象，所以需要用Map来接收
+        if(dataObj instanceof String){
+            String dataStr = (String) dataObj;
+            // 使用 Hutool 的 JSONUtil 将 JSON 字符串解析为 Map
+            return JSONUtil.toBean(dataStr, Map.class);
+        }else{
+            // 如果 dataObj 已经是 Map 类型或其他类型，则根据实际情况处理
+            // 这里假设我们期望的是 Map 类型，如果不是则需要做相应的类型转换或处理
+            return  (Map<String, Object>) dataObj;
+        }
     }
 }
