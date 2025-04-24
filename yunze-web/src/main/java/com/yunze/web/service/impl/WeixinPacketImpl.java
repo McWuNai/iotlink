908b6e2d019223171f3a1d100ce3e89151c416ae
McWuNai <3331547937@qq.com>
2025-04-24 17:35:19 +0800

    update: 规范键命名

--- yunze-web/src/main/java/com/yunze/web/service/impl/WeixinPacketImpl.java
+++ yunze-web/src/main/java/com/yunze/web/service/impl/WeixinPacketImpl.java
@@ -52,6 +52,7 @@ import java.util.stream.Collectors;
 @Slf4j
 @Component
 public class WeixinPacketImpl implements IWeixinPacket {
+    private final String queryFlowInfo_Key = "yunze:card:queryFlowInfo:";
 
     @Resource
     private CardFlowSyn cardFlowSyn;
@@ -101,8 +102,9 @@ public class WeixinPacketImpl implements IWeixinPacket {
         if (IccidMap.get("iccid") != null && IccidMap.get("iccid").toString().length() > 0) {
             String iccid = IccidMap.get("iccid").toString();
             Rmap.put("iccid", iccid);
-            String EndTime = "-";
-            Map<Object, Object> obj = redisUtilYz.redisTemplate.opsForHash().entries(iccid + openid);
+            String EndTime;
+            String redisFlowKey = queryFlowInfo_Key + iccid + ":" + openid;
+            Map<Object, Object> obj = redisUtilYz.redisTemplate.opsForHash().entries(redisFlowKey);
             if (obj.size() != 0) {
                 Double used = IccidMap.get("used") == null ? 0.00 : Double.parseDouble(IccidMap.get("used").toString());
                 Double remain = IccidMap.get("remaining") == null ? 0.00 : Double.parseDouble(IccidMap.get("remaining").toString());
@@ -190,10 +192,11 @@ public class WeixinPacketImpl implements IWeixinPacket {
                 Rmap.put("used", R_used);
                 Rmap.put("remain", R_remain);
                 Rmap.put("total", total);
-                Rmap.put("imsi", IccidMap.get("imsi") != null ? IccidMap.get("imsi") : "--");
+                Rmap.put("imsi", IccidMap.get("vid") != null ? IccidMap.get("vid") : "--");
                 Rmap.put("code", "200");
-                redisUtilYz.redisTemplate.opsForHash().putAll(iccid + openid, Rmap);
-                redisUtilYz.redisTemplate.expire(iccid + openid, 60, TimeUnit.SECONDS);
+                Rmap.put("activateDate", IccidMap.get("activate_date") != null ? IccidMap.get("activate_date") : "--");
+                redisUtilYz.redisTemplate.opsForHash().putAll(redisFlowKey, Rmap);
+                redisUtilYz.redisTemplate.expire(redisFlowKey, 60, TimeUnit.SECONDS);
             }
         } else {
             Rmap.put("code", "500");
@@ -1032,10 +1035,10 @@ public class WeixinPacketImpl implements IWeixinPacket {
             try {
                 if (yzWeChatUserImpl.upd(Updmap) > 0) {
                     Rmap.put("code", "200");
-                    Rmap.put("Message", "解绑成功！");
+                    Rmap.put("Message", "登出成功！");
                 }
             } catch (Exception e) {
-                Rmap.put("Message", "解绑异常！" + e.getMessage());
+                Rmap.put("Message", "登出异常！" + e.getMessage());
             }
         } else {
             Rmap.put("code", "500");
@@ -1062,7 +1065,7 @@ public class WeixinPacketImpl implements IWeixinPacket {
             String iccid = IccidMap.get("iccid").toString();
             map.put("iccid", iccid);
             String cd_operator_type = yzCardServiceImpl.findOperatorType(map);
-            String cardType = "默认";
+            String cardType = "联通";
             if (cd_operator_type != null && cd_operator_type.length() > 0) {
                 cardType = cd_operator_type;
             }
